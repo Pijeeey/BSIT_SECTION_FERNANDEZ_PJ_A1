925c777c22d227821aa4e5666198771022392ea9
Pijeeey <fernandez.pj10@gmail.com>
2025-02-09 23:44:04 +0800

    PRELIM_HO1_Web_Assessment
    Fernandez-A1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function showHobbies() {
+    alert("My hobbies include CODING, PLAYING PC GAAMES, ART STUFFS, and COOKING!");
+}
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'none'
+}
